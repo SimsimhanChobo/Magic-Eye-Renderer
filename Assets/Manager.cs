5226c514536d61be5f5eb85524891a6e62a98616
SimsimhanChobo <jun32820@gmail.com>
2022-10-27 00:04:41 +0900

    드래그 앤 드랍 지원

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -1,4 +1,6 @@
+using B83.Win32;
 using SFB;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +8,7 @@ using UnityEngine.Video;
 
 public class Manager : MonoBehaviour
 {
+    static readonly string[] textureExtension = new string[] { "tga", "targa", "dds", "png", "jpg", "jif", "jpeg", "jpe", "bmp", "exr", "gif", "hdr", "iff", "pict", "tif", "tiff", "psd", "ico", "jng", "koa", "lbm", "mng", "pbm", "pcd", "pcx", "pgm", "ppm", "ras", "wbpm", "cut", "xbm", "xpm", "g3", "sgi", "j2k", "j2c", "jp2", "pfm", "webp", "jxr" };
     static readonly string[] videoExtension = new string[] { "asf", "avi", "dv", "m4v", "mov", "mp4", "mpg", "mpeg", "ogv", "vp8", "webm", "wmv" };
     static readonly string[] allExtension = new string[] { "tga", "targa", "dds", "png", "jpg", "jif", "jpeg", "jpe", "bmp", "exr", "gif", "hdr", "iff", "pict", "tif", "tiff", "psd", "ico", "jng", "koa", "lbm", "mng", "pbm", "pcd", "pcx", "pgm", "ppm", "ras", "wbpm", "cut", "xbm", "xpm", "g3", "sgi", "j2k", "j2c", "jp2", "pfm", "webp", "jxr", "asf", "avi", "dv", "m4v", "mov", "mp4", "mpg", "mpeg", "ogv", "vp8", "webm", "wmv" };
     static readonly ExtensionFilter[] extensionFilters = new ExtensionFilter[] { new ExtensionFilter("Image Files ", allExtension)};
@@ -18,27 +21,50 @@ public class Manager : MonoBehaviour
     [SerializeField] AspectRatioFitter leftFitter;
     [SerializeField] AspectRatioFitter rightFitter;
 
+    void OnEnable()
+    {
+        UnityDragAndDropHook.InstallHook();
+        UnityDragAndDropHook.OnDroppedFiles += RefreshDragAndDrop;
+    }
+
+    void OnDisable()
+    {
+        UnityDragAndDropHook.UninstallHook();
+        UnityDragAndDropHook.OnDroppedFiles -= RefreshDragAndDrop;
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             string[] paths = StandaloneFileBrowser.OpenFilePanel("Open", "", extensionFilters, false);
             if (paths.Length > 0)
+                Refresh(paths[0]);
+        }
+    }
+
+    void RefreshDragAndDrop(List<string> paths, POINT point) => Refresh(paths[0]);
+
+
+    void Refresh(string path)
+    {
+        videoPlayer.Stop();
+
+        for (int i = 0; i < videoExtension.Length; i++)
+        {
+            if (Path.GetExtension(path) == "." + videoExtension[i])
             {
-                videoPlayer.Stop();
+                videoPlayer.url = "file://" + path;
+                videoPlayer.Play();
 
-                string path = paths[0];
-                for (int i = 0; i < videoExtension.Length; i++)
-                {
-                    if (Path.GetExtension(path) == "." + videoExtension[i])
-                    {
-                        videoPlayer.url = "file://" + path;
-                        videoPlayer.Play();
+                return;
+            }
+        }
 
-                        return;
-                    }
-                }
-
+        for (int i = 0; i < textureExtension.Length; i++)
+        {
+            if (Path.GetExtension(path) == "." + textureExtension[i])
+            {
                 Texture2D texture = new Texture2D(1, 1, TextureFormat.RGBA32, false);
                 AsyncImageLoader.LoadImage(texture, File.ReadAllBytes(path));
                 texture.filterMode = FilterMode.Point;
